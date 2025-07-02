fd0ba6e8abe6733b994f3c395b9273243c330447
yealim <ygim98218@daum.net>
2025-07-02 17:34:57 +0900

    헤더 css 수정

---
+++ src/app/components/layout/Footer.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from "react";
+
+export default function Footer(){
+    return(
+        <footer className="bg-gray-100 border-t mt-8">
+            <div className="max-w-screen-xl mx-auto px-4 py-6 text-center text-sm text-gray-600">
+                © 2025 대선 지도. All rights reserved.
+            </div>
+        </footer>
+    );
+};
---
+++ src/app/components/layout/Header.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from "react";
+
+export default function Header() {
+    return (
+    <header className="bg-gradient-to-br from-pink-700 via-violet-800 to-indigo-800">
+      <div className="text-white max-w-screen-xl mx-auto flex items-center justify-center p-20">
+            <h1 className="text-6xl font-bold text-center">2025 제 21대 대선 지도</h1>
+      </div>
+    </header>
+    );
+}
