8c9984ac0a965d4066573205f8cbcc5aff3ef99f
DevONew <kyl2778@naver.com>
2025-07-04 22:26:46 +0900

    브랜치 생성 및 컴포넌트 추가

---
+++ src/app/components/ui/CandidateCard.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from "react"
+
+export default function CandidateCard() {
+    return (
+        <div>
+            <div>
+                퍼블리싱중
+            </div>
+        </div>
+    );
+}
