55428369edb2d35759a5ef813cbc3456c397bc9b
DevONew <kyl2778@naver.com>
2025-07-14 23:19:28 +0900

    퍼센트 바 게이지 추가

--- src/app/components/ui/PercentageBar.tsx
+++ src/app/components/ui/PercentageBar.tsx
@@ -1,5 +1,6 @@
 'use client'
 import React from "react";
+import { useEffect, useState } from 'react';
 
 export type PercentageBarProps = {
     percentage: number; 
@@ -7,11 +8,21 @@ export type PercentageBarProps = {
 };
 
 export default function PercentageBar({percentage, color}:PercentageBarProps){
+    const [animatedWidth, setAnimatedWidth] = useState(0);
+
+    useEffect(() => {
+        // 마운트 이후에만 퍼센트 적용
+        const timeout = setTimeout(() => {
+        setAnimatedWidth(percentage);
+        }, 100); // 살짝 딜레이 줘야 transition 보임
+
+        return () => clearTimeout(timeout);
+    }, [percentage]);
     return (
         <div className="w-full h-1.5 bg-gray-200 rounded overflow-hidden">
             <div className="h-full transition-all duration-300 ease-in-out" 
                 style={{
-                width: `${percentage}%`,
+                width: `${animatedWidth}%`,
                 backgroundColor: color,
                 }}>
             </div>
